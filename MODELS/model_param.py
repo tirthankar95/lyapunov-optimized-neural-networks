06ec2bfd79c6f22c4c27cfe683256706d4cec9ef
tirthankar95 <tirthankarmittra@gmail.com>
2024-09-29 17:44:23 +0530

    Final Commit

--- MODELS/model_param.py
+++ MODELS/model_param.py
@@ -1,5 +1,5 @@
 n_children = 25
-epochs = 10
+epochs = 5
 learning_rate = 0.01
 std = 0.0001
 loss_lb, loss_ub = 0.2, 0.4
@@ -7,3 +7,7 @@ loss_lb, loss_ub = 0.2, 0.4
 def set_lr(val: int):
     global learning_rate
     learning_rate = val
+
+def set_epochs(val: int):
+    global epochs
+    epochs = val
